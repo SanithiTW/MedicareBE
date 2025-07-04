1c1ef161d35bc903702a64694b28b78dae12ac93
AshenKavinda <kavindahemarathna321@gmail.com>
2025-07-04 17:52:03 +0530

    re touch login page

--- app/src/main/java/com/example/donorlk/MainActivity.kt
+++ app/src/main/java/com/example/donorlk/MainActivity.kt
@@ -12,23 +12,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        enableEdgeToEdge()
-//        setContentView(R.layout.my_reservations)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 //            insets
 //        }
-        // 1. Create an Intent to start HomePageController
-        // The '.class.java' is required to reference the class for the Intent.
-        val intent = Intent(this, HomePageController::class.java)
-
-        // 2. Start the HomePageController activity
-        startActivity(intent)
-
-        // 3. Finish the MainActivity so the user cannot navigate back to it.
-        // This effectively makes HomePageController the first screen of your app.
-        finish()
+//        // 1. Create an Intent to start HomePageController
+//        // The '.class.java' is required to reference the class for the Intent.
+//        val intent = Intent(this, HomePageController::class.java)
+//
+//        // 2. Start the HomePageController activity
+//        startActivity(intent)
+//
+//        // 3. Finish the MainActivity so the user cannot navigate back to it.
+//        // This effectively makes HomePageController the first screen of your app.
+//        finish()
 
     }
 }
