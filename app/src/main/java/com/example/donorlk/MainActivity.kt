c63514ebeaf87e1aefb1cfd11090d1acfd2c611a
AshenKavinda <kavindahemarathna321@gmail.com>
2025-07-30 11:41:31 +0530

    edit activity

--- app/src/main/java/com/example/donorlk/MainActivity.kt
+++ app/src/main/java/com/example/donorlk/MainActivity.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.donorlk.controllers.HomePageController
 import com.example.donorlk.controllers.LoginController
+import com.example.donorlk.controllers.RegistrationController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Start with LoginController
-        val intent = Intent(this, LoginController::class.java)
+        val intent = Intent(this, RegistrationController::class.java)
         startActivity(intent)
 
         // Finish MainActivity so user cannot navigate back
