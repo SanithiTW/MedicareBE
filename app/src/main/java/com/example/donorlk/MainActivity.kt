b1dd52ee0045f7959cd3a7ef13341bd7d16e4d22
AshenKavinda <kavindahemarathna321@gmail.com>
2025-08-06 15:10:31 +0530

    admin dashboard

--- app/src/main/java/com/example/donorlk/MainActivity.kt
+++ app/src/main/java/com/example/donorlk/MainActivity.kt
@@ -3,18 +3,15 @@ package com.example.donorlk
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.donorlk.controllers.AddDonationCenterActivity
-import com.example.donorlk.controllers.HomePageController
 import com.example.donorlk.controllers.LoginController
-import com.example.donorlk.controllers.RegistrationController
-import com.example.donorlk.controllers.AdminDashboardController
+import com.example.donorlk.controllers.SubAdminHomeController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // Start with LoginController
-        val intent = Intent(this, AddDonationCenterActivity::class.java)
+        val intent = Intent(this, LoginController::class.java)
         startActivity(intent)
 
         // Finish MainActivity so user cannot navigate back
